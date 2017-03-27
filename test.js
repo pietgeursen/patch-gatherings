2ed01405f395ff596dc2f7f8385d747fac7749b7
Piet Geursen <pietgeursen@gmail.com>
2017-03-27 15:52:41 +1300

    Write deptest another way

--- test.js
+++ test.js
@@ -1,12 +1,26 @@
 const bulk = require('bulk-require')
 const combine = require('depject')
-const {map} = require('depject/apply')
+const nest = require('depnest')
+const {first} = require('depject/apply')
 const test = require('pull-test')
 
-const modules = combine(bulk(__dirname, ['!(node_modules|html|router|async)/**/*.js']))
+const depTest = {
+  gives: nest('runTests'),
+  needs: nest({
+    'test': 'map',
+  }),
+  create: function (api) {
+    return nest('runTests', function() {
+      const tests = api.test()
+      const reducedTests = tests.reduce((test, obj) => Object.assign(obj, test), {})
+      test(reducedTests)
+    })
+  }
+}
 
-const tests = map(modules.test)()
-const reducedTests = tests.reduce((test, obj) => Object.assign(obj, test), {})
-test(reducedTests)
+const modules = combine(bulk(__dirname, ['!(node_modules|html|router|async)/**/*.js']), depTest)
+
+
+first(modules.runTests)()
 
 
