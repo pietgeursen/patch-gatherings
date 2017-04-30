4338d6f59d2656f01f3f28a1805f91bcf0751b19
Piet Geursen <pietgeursen@gmail.com>
2017-04-30 18:46:57 +1200

    Fix thumbnail when no url

--- gathering/html/thumbnail.js
+++ gathering/html/thumbnail.js
@@ -1,5 +1,5 @@
 const nest = require('depnest')
-const { h, computed } = require('mutant')
+const { h, computed, when } = require('mutant')
 
 exports.needs = nest({
   'blob.sync.url': 'first'
@@ -11,6 +11,11 @@ exports.create = (api) => {
   return nest('gathering.html.thumbnail', thumbnail)
   function thumbnail ({ obs, msg }) {
     // TODO: Take the last published image as the thumbnail for now. Soon: Make thumbnail message so you can choose one.
-    return h('img', { src: computed([obs.images], (images) => api.blob.sync.url(images[images.length - 1])) })
+    const src = computed(obs.images, images => {
+      return images.length
+        ? api.blob.sync.url(images[images.length - 1])
+        : 'data:image/gif;base64,R0lGODlhAQABAPAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=='
+    })
+    return when(src, h('img', {src}))
   }
 }
